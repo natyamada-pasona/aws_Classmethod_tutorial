6bff9dc02194773ff83c005a345621387f07396b
natyamada-pasona <natsumi.yamada@dtc.work>
2026-03-18 10:58:55 +0900

    [update]　ディレクトリ整理

---
+++ 第一回/wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_BwvtbrHd1',
+        userPoolClientId: '3bpj8v4vtlqsilvoefb2jig3r7',
+        region: 'ap-northeast-1'
+    },
+    api: {
+        invokeUrl: 'https://6ak19ptgvl.execute-api.ap-northeast-1.amazonaws.com/prod'
+    }
+};
